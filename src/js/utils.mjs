3f00a4cb6f8d2d960c354e3f47c9f3b0f427020e
Tim <tomlinsont@byui.edu>
2023-05-06 09:28:57 -0600

    testing

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,10 +9,12 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
