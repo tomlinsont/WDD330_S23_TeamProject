7ca9193d9b3531f2e8863f05b1bbfb4ff8d38694
Diego Feresin <diegoferesin@gmail.com>
2023-05-16 23:33:43 -0300

    Team activity for this week

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import { productList } from './productList.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 
 console.log(productList('.product-list', 'tents'));
+loadHeaderFooter();
 //productList(".product-list", "tents");
