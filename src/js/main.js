31f842fa2cb96384f73d47ec0217dce40d613779
Tim <tomlinsont@byui.edu>
2023-05-19 10:01:16 -0600

    updated lint, format, and build

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import  productList  from './productList.mjs';
-import { loadHeaderFooter } from './utils.mjs';
+import productList from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 //console.log(productList('.product-list', 'tents'));
 loadHeaderFooter();
