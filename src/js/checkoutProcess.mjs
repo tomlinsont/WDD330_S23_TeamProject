b1ed0bb8d78d91bb4b48a85784f4f50d806c0b57
Tim <tomlinsont@byui.edu>
2023-06-06 17:11:50 -0600

    removed a comment

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -87,12 +87,6 @@ const checkoutProcess = {
       setLocalStorage("so-cart", []);
       location.assign("/checkout/success.html");
     } catch (err) {
-       // get rid of any preexisting alerts.
-      /* removeAllAlerts();
-       for (let message in err.message) {
-         alertMessage(err.message[message]);
-       }*/
-
       console.log(err);
     }
   },
