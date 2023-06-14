e3acd54e546d6e7cd6072f2b607625a993f5dcaf
Tim <tomlinsont@byui.edu>
2023-06-13 19:56:02 -0600

    week 9 team project

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,7 +1,7 @@
-const baseURL = 'https://wdd330-backend.onrender.com/';
-// const baseURL = 'http://server-nodejs.cit.byui.edu:3000/checkout';
+//const baseURL = 'https://wdd330-backend.onrender.com/';
+ //const baseURL = 'http://server-nodejs.cit.byui.edu:3000/';
 
-//const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL
 
 async function convertToJson(res) {
   if (res.ok) {
@@ -47,4 +47,30 @@ export async function checkout(payload) {
     body: JSON.stringify(payload),
   };
   return await fetch(baseURL + "checkout/", options).then(convertToJson);
+}
+
+export async function loginRequest(user) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  const response = await fetch(baseURL + "login", options).then(convertToJson);
+  return response.accessToken;
+}
+
+// make a request to the server for the current orders
+// requires: a valid token
+// returns: a list of orders
+export async function getOrders(token) {
+  const options = {
+    method: "GET",
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  };
+  const response =  await fetch(baseURL + "orders", options).then(convertToJson);
+  return response;
 }
---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+import { login } from './auth.mjs';
+import { getParam, loadHeaderFooter } from './utils.mjs';
+
+loadHeaderFooter();
+
+const redirect = getParam("redirect");
+
+document.querySelector("#loginSubmit").addEventListener("click", (e) => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    login({email, password}, redirect);
+});
