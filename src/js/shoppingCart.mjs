5d796b30c4628669b58c85485c9f998d08e3ef9b
Valentinabass <bas21003@byui.edu>
2023-07-11 09:35:02 -0300

    all

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -24,6 +24,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="delete-product">❌</span>
 </li>`;
 
   return newItem;
