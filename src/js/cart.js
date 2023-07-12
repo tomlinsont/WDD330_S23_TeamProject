90f9864a2050b67fdc963ffa6503b77e096822fd
Valentinabass <bas21003@byui.edu>
2023-07-12 13:44:06 -0300

    df

--- src/js/cart.js
+++
@@ -1,4 +0,0 @@
-import { loadHeaderFooter } from "./utils.mjs";
-import ShoppingCart from "./shoppingCart.mjs";
-
-loadHeaderFooter().then((res) => ShoppingCart());
--- src/js/product.js
+++
@@ -1,8 +0,0 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
-
-loadHeaderFooter();
-
-const productId = getParam("product");
-
-productDetails(productId);
